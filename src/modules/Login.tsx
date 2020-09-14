bc8e76e5f73e02d3e2cbf81efedfac8ce2ded8e1
mitchon <soviet_uwu@outlook.com>
2020-09-14 22:43:03 +0300

    new file:   package-lock.json 	new file:   src/modules/Login.tsx 	modified:   package.json 	deleted:    src/App.css 	modified:   src/App.tsx 	modified:   src/index.tsx 	deleted:    src/logo.svg 	modified:   tsconfig.json

---
+++ src/modules/Login.tsx
@@ -0,0 +1,57 @@
+import React, {useRef} from 'react';
+import {gql, useMutation} from '@apollo/client';
+
+export const Login = ({logInHandler}) => {
+    const loginInput = useRef('');
+    const passwordInput = useRef('');
+
+    const SignIn = gql`
+    mutation ($login: String!, $password: String!){
+        signIn(input: { login: $login, password: $password }) {
+          id
+          login
+        }
+      }
+  `;
+
+    const [signIn] = useMutation(SignIn);
+
+    const buttonHandler = (): any => {
+        if (loginInput.current.value && passwordInput.current.value)
+        {
+            signIn({variables: {
+                login: loginInput.current.value,
+                password: passwordInput.current.value
+            }}).then(res => {
+                if (res.data.signIn) {
+                    localStorage.setItem('User', res.data.signIn);
+                    logInHandler(true);
+                }
+                else alert("Incorrect login or password")
+            })
+        }
+        else {
+            alert('Incorrect login or password');
+        }
+    }
+
+    return (
+        <>
+            <div>
+                <input
+                    type="text"
+                    placeholder="Login"
+                    ref={loginInput}
+                />
+            </div>
+            <div>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    ref={passwordInput}
+                />
+            </div>
+            <button onClick={buttonHandler}>Сюда жми!</button>
+        </>
+    )
+}
